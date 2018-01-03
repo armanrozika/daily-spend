8f0f0e7162ddee6bf8c8689efe79e9eef605082f
armanrozika <padahariyanglain@gmail.com>
2018-01-03 21:47:01 +0700

    dependencies

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+let gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	browserSync = require('browser-sync'),
+	webpack = require('webpack-stream');
+
+gulp.task('sass', function(){
+	
+});
