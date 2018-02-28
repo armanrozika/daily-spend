875671e3ca45a835cd72c644ba2ded272ab031f1
armanrozika <padahariyanglain@gmail.com>
2018-02-28 10:02:05 +0700

    Update js1.js

--- src/js/js1.js
+++ src/js/js1.js
@@ -5,33 +5,33 @@ var getYear = document.getElementById('year');
 
 var addButton = document.getElementById('button');
 
-var store = {};
+//var store = {};
 var dbContent = firebase.database().ref('messages');
 
 
 addButton.addEventListener('click', function(){
-	var item = document.getElementById('item');
-	var price = document.getElementById('harga');
+	var item = document.getElementById('item').value;
+	var price = document.getElementById('harga').value;
 	var day = getDay.options[getDay.selectedIndex].value;
 	var month = getMonth.options[getMonth.selectedIndex].value;
 	var year = getYear.options[getYear.selectedIndex].value;
 	//alert(item.value);
 	
-	var obj = {
-		hari: day,
-		bulan: month,
-		tahun: year,
-		barang: item.value,
-		harga: price.value
-	};
+// 	var obj = {
+// 		hari: day,
+// 		bulan: month,
+// 		tahun: year,
+// 		barang: item.value,
+// 		harga: price.value
+// 	};
 
-	if(store.length>0){
-		store = {};
-	};
+// 	if(store.length>0){
+// 		store = {};
+// 	};
 
-	store.input = obj;
-	var string = JSON.stringify(store);
-	saveMessage(store.input.hari, store.input.bulan, store.input.tahun, store.input.barang, store.input.harga);
+// 	store.input = obj;
+// 	var string = JSON.stringify(store);
+	saveMessage(day, month, year, item, price);
 	//console.log();
 
 	//empty the input value 
@@ -51,6 +51,14 @@ function saveMessage(hari, bulan, tahun, barang, harga){
     harga: harga
   });
 }
+
+//I think I found a better way, eliminate unnecessary code
+//I don't have to make an empty oject outside eventlistener, I don't have to make empty object anywhere.
+//I am storing the data in firebase, the saveMessage Function make the object to store the data.
+//Try to do JSON stringify inside saveMessage Function,
+
+
+
 
 
 
